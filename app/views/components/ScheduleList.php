03148eec8fe729734dfded7e91633617c7256217
Adam Moore <adamaoc@gmail.com>
2018-05-25 15:13:26 -0500

    update responsive styling

--- app/views/components/ScheduleList.php
+++ app/views/components/ScheduleList.php
@@ -1,13 +1,16 @@
 <ul>
-  <?php foreach ($data as $key => $game) : ?>
-    <li>
-      <a href="/schedule/<?= $game['id'] ?>" class="schedule-list__icon">
-        <?php getComponent('LinkIcon'); ?>
-      </a>
-      <span class="schedule-list__date"><?= $game['date'] ?></span>
-      <span class="schedule-list__teams">
-        <?= $game['home_team'] ?> vs <?= $game['visiting_team'] ?>
-      </span>
-    </li>
-  <?php endforeach; ?>
+  <?php
+    if (!empty($data)) :
+      foreach ($data as $key => $game) : ?>
+      <li>
+        <a href="/schedule/<?= $game['id'] ?>" class="schedule-list__icon">
+          <?php getComponent('LinkIcon'); ?>
+        </a>
+        <span class="schedule-list__date"><?= $game['date'] ?></span>
+        <span class="schedule-list__teams">
+          <?= $game['home_team'] ?> vs <?= $game['visiting_team'] ?>
+        </span>
+      </li>
+    <?php endforeach; ?>
+  <?php endif; ?>
 </ul>
