bd703950c4ac3a4c14617061271092b076cc3769
Adam Moore <adamaoc@gmail.com>
2018-07-13 09:13:59 -0500

    update alt text var

--- app/views/components/Sponsors.php
+++ app/views/components/Sponsors.php
@@ -5,7 +5,7 @@
   <div class="sponsors__logos">
     <ul>
       <?php foreach ($data as $sponsor) : ?>
-        <li><img src="/public/images/sponsors/<?= $sponsor['image'] ?>" alt="<?= $sponsor['img_alt'] ?>" /></li>
+        <li><img src="/public/images/sponsors/<?= $sponsor['image'] ?>" alt="<?= $sponsor['image_alt'] ?>" /></li>
       <?php endforeach; ?>
     </ul>
   </div>
