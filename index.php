547b6152dd21bd13f710b68484acc7185ca4310c
adam moore <adammoore@adams-iMac.local>
2018-11-12 16:35:52 -0600

    hide errors in production

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 
-//to Hide All Errors:
-// error_reporting(0);
-// ini_set('display_errors', 0);
-
-//to Show All Errors:
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', TRUE);
+if ($_SERVER['HTTP_HOST'] === 'localhost:8888') {
+    //to Show All Errors:
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', TRUE);
+} else {
+    //to Hide All Errors:
+    error_reporting(0);
+    ini_set('display_errors', 0);
+}
 
 require_once 'app/init.php';
 
